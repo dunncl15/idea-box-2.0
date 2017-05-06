9247ccf4be760f2a793a5bc431115de4e653d588
Charles Dunn <dunnc1551@gmail.com>
2017-05-06 17:04:20 -0600

    Add functionality to save new ideas and delete existing ideas.

--- public/app.js
+++ public/app.js
@@ -1,16 +1,83 @@
 
 $(document).ready(() => {
-  fetch('api/v1/folders')
-  .then(response => response.json())
-  .then(folders => renderFolders(folders));
+  getAllFolders();
+  getAllIdeas();
 });
 
-const renderFolders = (folders) => {
-  return folders.map(folder => appendFolders(folder))
+$('.save-btn').on('click', function(e) {
+  e.preventDefault();
+  let $title = $('.user-title').val();
+  let $body = $('.user-body').val();
+  saveIdea($title, $body);
+  clearInputs();
+});
+
+$('.idea-folder--all').on('click', () => {
+  renderIdeas();
+})
+
+$('.ideas').on('click', '.delete-btn', function() {
+  let id = ($(this).closest('.idea-container').attr('id'));
+  deleteIdea(id)
+})
+
+const getAllFolders = () => {
+  fetch('/api/v1/folders')
+  .then(response => response.json())
+  .then(folders => renderData(folders, append));
 }
 
-const appendFolders = (folder) => {
-  $('.idea-folders').append(`<button class="idea-folder">${folder.title}</button>`)
+const getAllIdeas = () => {
+  fetch('/api/v1/ideas')
+  .then(response => response.json())
+  .then(ideas => renderData(ideas, append));
+}
+
+const saveIdea = (title, body) => {
+  fetch('/api/v1/ideas', {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({ title: title, body: body, folder_id: 3 })
+  })
+  .then(response => renderIdeas())
+  .catch(error => console.error('error: ', error))
+}
+
+const deleteIdea = (id) => {
+  fetch(`/api/v1/ideas/${id}`, {
+    method: 'DELETE'
+  })
+  .then(response => renderIdeas())
+  .catch(error => console.error('error: ', error))
+}
+
+const renderData = (data, append) => {
+  return data.map(data => append(data))
+}
+
+const append = (data) => {
+  const { title, body, id } = data;
+  if (body) {
+    $('.ideas').append(`
+    <div id="${id}" class="idea-container">
+     <textarea class="idea-title">${title}</textarea>
+     <textarea class="idea-body">${body}</textarea>
+     <button class="delete-btn"></button>
+     <div class="vote-icon-wrap">
+       <button class="upvote-btn"></button>
+       <button class="downvote-btn"></button>
+       <p class="quality">quality:</p>
+       <p class="quality-value">swill</p>
+     </div>
+    </div>`);
+  } else {
+    $('.idea-folders').append(`<button id="${id}" class="idea-folder">${title}</button>`)
+  }
+}
+
+const renderIdeas = () => {
+  $('.ideas').children().remove();
+  getAllIdeas();
 }
 
 //Event Listeners
@@ -20,13 +87,7 @@ $('input[type="text"], textarea').on('keyup', function() {
 });
 
 
-//Save Button Click Event
 
-$('.save-btn').on('click', function(e) {
-  e.preventDefault();
-  getIdea();
-  clearInputs();
-});
 
 //Upvote Click Event
 
@@ -68,14 +129,6 @@ $('.idea-section').on('click', '.downvote-btn', function() {
   sendToStorage(id, storedObject);
 })
 
-//Delete Button Click Event
-
-$('.idea-section').on('click', '.delete-btn', function() {
-  $(this).parent('.idea-container').remove();
-  var id = $(this).parent('.idea-container').attr('id');
-  removeFromStorage(id);
-})
-
 //Blur Event
 
 $('.idea-section').on('blur', '.idea-title, .idea-body', function() {
@@ -101,46 +154,6 @@ $('.search-field').on('keyup', function() {
   })
 })
 
-//Functions
-
-function Idea(title, body) {
-  this.title = title;
-  this.body = body;
-  this.quality = 'swill';
-  this.id = Date.now();
-}
-
-function getIdea() {
-  var ideaTitle = $('.user-title').val();
-  var ideaBody = $('.user-body').val();
-  var userIdea = new Idea(ideaTitle, ideaBody);
-  addIdea(userIdea);
-  sendToStorage(userIdea.id, userIdea);
-}
-
-function addIdea(idea) {
-  $('.idea-section').prepend(`
-  <div id="${idea.id}" class="idea-container">
-   <textarea class="idea-title">${idea.title}</textarea>
-   <textarea class="idea-body">${idea.body}</textarea>
-   <button class="delete-btn"></button>
-   <div class="vote-icon-wrap">
-     <button class="upvote-btn"></button>
-     <button class="downvote-btn"></button>
-     <p class="quality">quality:</p>
-     <p class="quality-value">${idea.quality}</p>
-   </div>
-  </div>`);
-}
-
-function sendToStorage(id, object) {
-  localStorage.setItem(id, JSON.stringify(object));
-}
-
-function removeFromStorage(id) {
-  localStorage.removeItem(id);
-}
-
 function clearInputs() {
   $('.user-title').val('');
   $('.user-body').val('');
@@ -155,7 +168,6 @@ function checkIdeaForm() {
       enableBtn();
     }
   });
-  return complete;
 }
 
 function enableBtn() {
